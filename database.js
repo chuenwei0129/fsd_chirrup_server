4b2a1d242f599437d71774be754d175cae3639d6
chuenwei0129 <chuenwei0129@gmail.com>
2024-10-11 13:02:42 +0800

    初始化数据库

--- database.js
+++ database.js
@@ -1,15 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose()
 
-const DBSOURCE = 'db.sqlite';
+const DBSOURCE = 'db.sqlite'
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
-    if(err){
-        console.log(err.message);
-        throw err;
-    }else{
-        console.log('Connected to the SQLite database.')
+  if (err) {
+    console.log(err.message)
+    throw err
+  } else {
+    console.log('Connected to the SQLite database.')
 
-        db.run(`CREATE TABLE users (
+    db.run(
+      `CREATE TABLE users (
                 user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 first_name text,
                 last_name text,
@@ -18,61 +19,67 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
                 salt text,
                 session_token text,
                 CONSTRAINT username_unique UNIQUE (username)
-            )`, (err) => {
-                if(err){
-                    console.log('Users table already created');
-                }else{
-                    console.log('Users table created');
-                }
-            }
-        );
+            )`,
+      (err) => {
+        if (err) {
+          console.log('Users table already created')
+        } else {
+          console.log('Users table created')
+        }
+      }
+    )
 
-        
-        db.run(`CREATE TABLE posts (
+    db.run(
+      `CREATE TABLE posts (
                 post_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 text TEXT,
                 date_published INTEGER,
                 author_id INTEGER,
                 FOREIGN KEY(author_id) REFERENCES users(user_id)
-            )`, (err) => {
-                if(err){
-                    console.log('Posts table already created');
-                }else{
-                    console.log('Posts table created');
-                }
-            }
-        );
+            )`,
+      (err) => {
+        if (err) {
+          console.log('Posts table already created')
+        } else {
+          console.log('Posts table created')
+        }
+      }
+    )
 
-        db.run(`CREATE TABLE likes (
+    db.run(
+      `CREATE TABLE likes (
                 post_id INTEGER,
                 user_id INTEGER,
                 PRIMARY KEY (post_id, user_id),
                 FOREIGN KEY (post_id) REFERENCES posts(post_id),
                 FOREIGN KEY (user_id) REFERENCES users(user_id)
-            )`, (err) => {
-                if(err){
-                    console.log('Likes table already created');
-                }else{
-                    console.log('Likes table created');
-                }
-            }
-        );
+            )`,
+      (err) => {
+        if (err) {
+          console.log('Likes table already created')
+        } else {
+          console.log('Likes table created')
+        }
+      }
+    )
 
-        db.run(`CREATE TABLE followers (
+    db.run(
+      `CREATE TABLE followers (
                 user_id INTEGER,
                 follower_id INTEGER,
                 PRIMARY KEY (user_id, follower_id),
                 FOREIGN KEY (user_id) REFERENCES users(user_id),
                 FOREIGN KEY (follower_id) REFERENCES users(user_id)
-            )`, (err) => {
-                if(err){
-                    console.log('Followers table already created');
-                }else{
-                    console.log('Followers table created');
-                }
-            }
-        )
-    }
-});
+            )`,
+      (err) => {
+        if (err) {
+          console.log('Followers table already created')
+        } else {
+          console.log('Followers table created')
+        }
+      }
+    )
+  }
+})
 
-module.exports = db;
+module.exports = db
