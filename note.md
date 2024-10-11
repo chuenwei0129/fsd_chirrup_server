f7902827a2f026aab9a24cd04d31cd98a2876932
chuenwei0129 <chuenwei0129@gmail.com>
2024-10-11 13:05:39 +0800

    同步数据库到 prisma

---
+++ note.md
@@ -0,0 +1,1 @@
+![20241011130122](https://raw.githubusercontent.com/chuenwei0129/my-picgo-repo/master/react/20241011130122.png)
--- tests/database.refresh.js
+++ tests/database.refresh.js
@@ -1,64 +1,64 @@
-const db = require("../database");
+const db = require('../database')
 
 let count = 0
 
-console.log("****************************************")
-console.log("Deleting data...")  
+console.log('****************************************')
+console.log('Deleting data...')
 
 const sql = 'DELETE FROM followers'
 
-db.run(sql, [], function(err){
-    if(err) throw err
+db.run(sql, [], function (err) {
+  if (err) throw err
 
-    console.log("Followers: All data deleted")
+  console.log('Followers: All data deleted')
+  count++
+
+  const sql = 'DELETE FROM likes'
+
+  db.run(sql, [], function (err) {
+    if (err) throw err
+
+    console.log('Likes: All data deleted')
     count++
 
-    const sql = 'DELETE FROM likes'
+    const sql = 'DELETE FROM posts'
 
-    db.run(sql, [], function(err){
-        if(err) throw err
-    
-        console.log("Likes: All data deleted")
+    db.run(sql, [], function (err) {
+      if (err) throw err
+
+      console.log('Posts: All data deleted')
+      count++
+
+      const sql = 'DELETE FROM users'
+
+      db.run(sql, [], function (err) {
+        if (err) throw err
+
+        console.log('Users: All data deleted')
         count++
-    
-        const sql = 'DELETE FROM posts'
-    
-        db.run(sql, [], function(err){
-            if(err) throw err
-        
-            console.log("Posts: All data deleted")
+
+        const sql =
+          "UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'users'"
+
+        db.run(sql, [], function (err) {
+          if (err) throw err
+
+          console.log('Users: reset ID counter')
+          count++
+
+          const sql =
+            "UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'posts'"
+          db.run(sql, [], function (err) {
+            if (err) throw err
+
+            console.log('Posts: reset ID counter')
             count++
-        
-            const sql = 'DELETE FROM users'
-        
-            db.run(sql, [], function(err){
-                if(err) throw err
-            
-                console.log("Users: All data deleted")
-                count++
 
-                const sql = "UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'users'";
-
-                db.run(sql, [], function(err){
-                    if(err) throw err
-
-                    console.log("Users: reset ID counter")
-                    count++
-
-                    const sql = "UPDATE `sqlite_sequence` SET `seq` = 0 WHERE `name` = 'posts'";
-                    db.run(sql, [], function(err){
-                        if(err) throw err
-
-                        console.log("Posts: reset ID counter")
-                        count++
-
-                        console.log("All data deleted from all tables") 
-                        console.log("****************************************")
-                    })
-                })
-                
-                
-            })
+            console.log('All data deleted from all tables')
+            console.log('****************************************')
+          })
         })
+      })
     })
+  })
 })
